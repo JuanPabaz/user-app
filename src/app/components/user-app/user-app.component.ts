5a71298dca2fc66589afb1e16383d69e04ef837e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-03 17:10:43 -0500

    feat: import userComponent

--- src/app/components/user-app/user-app.component.ts
+++ src/app/components/user-app/user-app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-user-app',
-  imports: [],
+  imports: [UserComponent],
   templateUrl: './user-app.component.html',
   styleUrl: './user-app.component.css'
 })
