049bba7accd0738e4ae000cd5bda3a0441d0acb0
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-03 17:28:48 -0500

    feat: implement addUser

--- src/app/components/user-app/user-app.component.ts
+++ src/app/components/user-app/user-app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { UserComponent } from '../user/user.component';
+import { UserFormComponent } from '../user-form/user-form.component';
 
 @Component({
   selector: 'app-user-app',
-  imports: [UserComponent],
+  imports: [UserComponent, UserFormComponent],
   templateUrl: './user-app.component.html',
   styleUrl: './user-app.component.css'
 })
@@ -20,5 +21,9 @@ export class UserAppComponent implements OnInit{
     this.user_service.findAllUsers().subscribe(users => 
       this.users = users
     );
+  }
+
+  addUser(user: User){
+    this.users = [...this.users,user];
   }
 }
