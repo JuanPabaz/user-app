99e0c563fffbde71e267837b7842aedb3d984a76
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-03 17:28:57 -0500

    feat: send output

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms' 
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
+
+  user: User = new User();
+
+  @Output() newUserEventEmitter: EventEmitter<User> = new EventEmitter();
+
+  constructor(){
+    this.user = new User();
+  }
+
+  onSubmit(){
+    this.newUserEventEmitter.emit(this.user)
+    console.log(this.user);
+  }
 
 }
