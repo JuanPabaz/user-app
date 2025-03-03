9594d0d2d8333c0c0a20a6c9828e23609b20494e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-03 17:06:35 -0500

    feat: user data and findAllUsers method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private users: User[] = [
+    {
+      id:1,
+      name: 'Juan Pablo',
+      lastName: 'Giraldo',
+      email: 'jpgthf@gmail.com',
+      username: 'juanpablo24',
+      password: 'coco123'
+    },
+    {
+      id:2,
+      name: 'Manuela',
+      lastName: 'Zapata',
+      email: 'manu@gmail.com',
+      username: 'manu1',
+      password: 'manu123'
+    },
+    {
+      id:3,
+      name: 'Jeronimo',
+      lastName: 'Arango',
+      email: 'jero@gmail.com',
+      username: 'jero1',
+      password: 'jero123'
+    },
+    {
+      id:4,
+      name: 'Carolina',
+      lastName: 'Hernandez',
+      email: 'caro@gmail.com',
+      username: 'psicologa1',
+      password: 'psicologa123'
+    }
+  ]
+
   constructor() { }
+
+  findAllUsers(): Observable<User[]>{
+    return of(this.users);
+  }
 }
