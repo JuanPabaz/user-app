952cb2060aab231e9958ce3233e3b7bcaddb55f2
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 16:43:23 -0500

    feat: delete unnecesary route implementation and change it for the backend implementation

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { Router, RouterModule } from '@angular/router';
 import { UserService } from '../../services/user.service';
@@ -10,7 +10,7 @@ import { SharingDataService } from '../../services/sharing-data.service';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
 
   users: User[] = [];
 
@@ -18,13 +18,12 @@ export class UserComponent {
     private sharing_data_service: SharingDataService,
     private user_service: UserService,
     private router: Router){
-    if (this.router.getCurrentNavigation()?.extras.state){
-      this.users = this.router.getCurrentNavigation()?.extras.state!['users'];
-    }else{
-      this.user_service.findAllUsers().subscribe(users => {
-        this.users = users;
-      });
-    }
+  }
+
+  ngOnInit(): void {
+    this.user_service.findAllUsers().subscribe(users => {
+      this.users = users;
+    });
   }
 
   onRemoveUser(id: number){
