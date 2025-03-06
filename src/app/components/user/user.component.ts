a8545c5f3b10161e81c5a80267bc29969c131306
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 08:31:31 -0500

    feat: get user list

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter } from '@angular/core';
 import { User } from '../../models/user';
 import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -16,8 +17,16 @@ export class UserComponent {
 
   selectedUserEventEmitter = new EventEmitter()
 
-  constructor(private router: Router){
-    this.users = this.router.getCurrentNavigation()?.extras.state!['users'];
+  constructor(
+    private user_service: UserService,
+    private router: Router){
+    if (this.router.getCurrentNavigation()?.extras.state){
+      this.users = this.router.getCurrentNavigation()?.extras.state!['users'];
+    }else{
+      this.user_service.findAllUsers().subscribe(users => {
+        this.users = users;
+      });
+    }
   }
 
   onRemoveUser(id: number){
