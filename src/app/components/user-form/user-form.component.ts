a0e875a68f30c3739f5f0b0acd1aa998c7f95832
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 09:08:46 -0500

    feat: implement state validation

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms' 
 import { User } from '../../models/user';
 import { SharingDataService } from '../../services/sharing-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
@@ -10,10 +11,16 @@ import { SharingDataService } from '../../services/sharing-data.service';
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
-  user: User = new User();
+  user: User;
 
-  constructor(private sharing_data_service: SharingDataService){
-    this.user = new User();
+  constructor(
+    private router: Router,
+    private sharing_data_service: SharingDataService){
+    if (this.router.getCurrentNavigation()?.extras.state){
+      this.user = this.router.getCurrentNavigation()?.extras.state!['user'];
+    }else{
+      this.user = new User();
+    }
   }
 
   onSubmit(userForm: NgForm){
