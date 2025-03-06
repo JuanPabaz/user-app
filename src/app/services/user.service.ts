da5365a244836132d1cdcb99c9192662d223542b
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 15:45:08 -0500

    feat: fetch data from backend

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,50 +1,17 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private users: User[] = [
-    {
-      id:1,
-      name: 'Juan Pablo',
-      lastName: 'Giraldo',
-      email: 'jpgthf@gmail.com',
-      username: 'juanpablo24',
-      password: 'coco123'
-    },
-    {
-      id:2,
-      name: 'Manuela',
-      lastName: 'Zapata',
-      email: 'manu@gmail.com',
-      username: 'manu1',
-      password: 'manu123'
-    },
-    {
-      id:3,
-      name: 'Jeronimo',
-      lastName: 'Arango',
-      email: 'jero@gmail.com',
-      username: 'jero1',
-      password: 'jero123'
-    },
-    {
-      id:4,
-      name: 'Carolina',
-      lastName: 'Hernandez',
-      email: 'caro@gmail.com',
-      username: 'psicologa1',
-      password: 'psicologa123'
-    }
-  ]
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   findAllUsers(): Observable<User[]>{
-    return of(this.users);
+    // return of(this.users);
+    return this.http.get<User[]>('http://localhost:8080/user');
   }
 }
