296d07588cfda09d2eaeb31c46838fabd661761e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-06 08:41:01 -0500

    feat: implement event emitters in the service

--- src/app/services/sharing-data.service.ts
+++ src/app/services/sharing-data.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharingDataService {
 
+  private _idUserEventEmitter = new EventEmitter()
+  
+  private _selectedUserEventEmitter = new EventEmitter()
+
+  private _newUserEventEmitter: EventEmitter<User> = new EventEmitter();
+
   constructor() { }
+
+  get idUserEventEmitter(): EventEmitter<number>{
+    return this._idUserEventEmitter;
+  }
+
+  get selectedUserEventEmitter(): EventEmitter<User>{
+    return this._selectedUserEventEmitter;
+  }
+
+  get newUserEventEmitter(): EventEmitter<User>{
+    return this._newUserEventEmitter;
+  }
 }
