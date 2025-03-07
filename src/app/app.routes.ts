c23f6940a221544a7c6ab2f89c69786e8654bf58
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-07 18:29:14 -0500

    feat: add users route with page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,10 @@ export const routes: Routes = [
         component: UserComponent
     },
     {
+        path:'users/page/:page',
+        component: UserComponent
+    },
+    {
         path:'create-user',
         component: UserFormComponent
     },
