92c8f3c492944ba0f305f641d0d7f5561876cada
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-08 12:23:14 -0500

    feat: create paginator template

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-paginator',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './paginator.component.html',
   styleUrl: './paginator.component.css'
 })
 export class PaginatorComponent {
-
+  @Input() url: string = '';
+  @Input() paginator: any = {};
+  
 }
