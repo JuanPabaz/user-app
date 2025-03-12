302e691fe511ab4a73a7ce58fb51a6c84fb7b55e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-12 16:00:41 -0500

    feat: add authenticated validation

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,27 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  inject(Router).navigate(['/login']);
+  const authService = inject(AuthService);
+  const router = inject(Router)
+  if (authService.isAuth()){
+    if (isTokenExpired()){
+      authService.logOut();
+      router.navigate(['/login']);    
+    }
+    return true;
+  }
+  router.navigate(['/login']);
   return false;
 };
+
+const isTokenExpired = () => {
+  const authService = inject(AuthService);
+  const token = authService.token;
+  const payload = authService.getPayload(token);
+  const exp = payload.exp;
+  const now = new Date().getTime()/1000;
+  
+  return exp < now ? true : false;
+}
