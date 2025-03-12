4b92477a4b4d78ba375ebaa6272b46d011d784dd
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-12 15:36:36 -0500

    feat: show create when admin and username when authenticated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,4 +12,12 @@ import { User } from '../../models/user';
 export class NavbarComponent {
   @Input() users:User[] = [];
   @Input() paginator = {};
+
+  constructor(private auth_service: AuthService){
+
+  }
+
+  get user(){
+    return this.auth_service.user;
+  }
 }
