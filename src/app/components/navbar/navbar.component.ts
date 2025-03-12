5846ad9f2b875614ffbeda845cbcc448cb31629d
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-12 15:44:17 -0500

    feat: add logout button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { User } from '../../models/user';
 import { AuthService } from '../../services/auth.service';
 
@@ -13,11 +13,17 @@ export class NavbarComponent {
   @Input() users:User[] = [];
   @Input() paginator = {};
 
-  constructor(private auth_service: AuthService){
-
-  }
+  constructor(
+    private auth_service: AuthService,
+    private router: Router)
+    {}
 
   get user(){
     return this.auth_service.user;
+  }
+
+  logoutHandler(){
+    this.auth_service.logOut();
+    this.router.navigate(['/login'])
   }
 }
